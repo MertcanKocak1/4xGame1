43c084fb4c2a18910c5695d56ec5640203e6a080
=MertcanKocak1 <=inuxe06@gmail.com>
2023-08-20 02:39:15 +0300

    Yoksayılan Dosyalar Gönderildi.

---
+++ PanteonGame/Entities/Log/LogDetail.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Log
+{
+    public class LogDetail
+    {
+        [Key]
+        public int Id { get; set; }
+        public int LogId { get; set; }
+        public string? DataBeforeOperation { get; set; }
+        public string? DataAfterOperation { get; set; }
+    }
+}
---
+++ PanteonGame/Entities/Log/LogError.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Log
+{
+    public class LogError
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? FunctionName { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? StackTrace { get; set; }
+        public DateTime DateOccured { get; set; }
+        public string ClassName { get; set; }
+        public string? MethodName { get; set; }
+
+    }
+}
